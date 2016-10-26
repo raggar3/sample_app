e333354f5085831d27c236d33323e7b0b7c34d2f
Henry263 <hrpatel.263@gmail.com>
2016-10-25 18:14:09 -0700

    Update gulpfile.js

--- gulpfile.js
+++ gulpfile.js
@@ -6,7 +6,7 @@ var sass = require('gulp-sass');
 var minifyCss = require('gulp-minify-css');
 var rename = require('gulp-rename');
 var sh = require('shelljs');
-//hello 
+// Adding comment
 var paths = {
   sass: ['./scss/**/*.scss']
 };
